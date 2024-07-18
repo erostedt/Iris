579d9f637f7df2062658782e6aba5fac10fac223
erostedt <rostedteric@gmail.com>
2024-07-18 16:50:40 +0200

    add commond directions

---
+++ src/core/Directions.hpp
@@ -0,0 +1,10 @@
+#include <glm/glm.hpp>
+
+static const glm::vec3 LEFT = {-1.0f, 0.0f, 0.0f};
+static const glm::vec3 RIGHT = {1.0f, 0.0f, 0.0f};
+
+static const glm::vec3 UP = {0.0f, 1.0f, 0.0f};
+static const glm::vec3 DOWN = {0.0f, -1.0f, 0.0f};
+
+static const glm::vec3 FORWARD = {0.0f, 0.0f, -1.0f};
+static const glm::vec3 BACKWARD = {0.0f, 0.0f, 1.0f};
