ae79f1f62a1b2a852d4e139f51912b28d3730a86
Eric Rostedt <rostedteric@gmail.com>
2024-07-23 11:52:17 +0200

    add white and black colors

--- src/core/Color.hpp
+++ src/core/Color.hpp
@@ -3,6 +3,8 @@
 
 using Color = glm::vec4;
 
+static inline Color WHITE{1.0f, 1.0f, 1.0f, 1.0f};
+static inline Color BLACK{0.0f, 0.0f, 0.0f, 1.0f};
 static inline Color RED{1.0f, 0.0f, 0.0f, 1.0f};
 static inline Color GREEN{0.0f, 1.0f, 0.0f, 1.0f};
 static inline Color BLUE{0.0f, 0.0f, 1.0f, 1.0f};
