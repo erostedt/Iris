67a0fc841b22ce6672d6932afabaa6a254cb677f
Eric Rostedt <rostedteric@gmail.com>
2024-07-23 11:52:27 +0200

    move directions into Iris namespace

--- src/core/Directions.hpp
+++ src/core/Directions.hpp
@@ -1,5 +1,8 @@
 #include <glm/glm.hpp>
 
+namespace Iris
+{
+
 static const glm::vec3 LEFT = {-1.0f, 0.0f, 0.0f};
 static const glm::vec3 RIGHT = {1.0f, 0.0f, 0.0f};
 
@@ -8,3 +11,5 @@ static const glm::vec3 DOWN = {0.0f, -1.0f, 0.0f};
 
 static const glm::vec3 FORWARD = {0.0f, 0.0f, -1.0f};
 static const glm::vec3 BACKWARD = {0.0f, 0.0f, 1.0f};
+
+}; // namespace Iris
