50a3cd83ef839e0b3d60c490b6d16459af5c0018
erostedt <rostedteric@gmail.com>
2024-10-26 22:33:07 +0200

    refactor: remove unused constructor

--- src/core/VertexArray.hpp
+++ src/core/VertexArray.hpp
@@ -15,12 +15,6 @@ template <typename T> class VertexArray : Buffer
     {
         glGenVertexArrays(1, &m_id);
     }
-    VertexArray(const VertexAttributeBuffer<T> &vertex_buffer)
-    {
-        glGenVertexArrays(1, &m_id);
-        AddVertexLayout(vertex_buffer);
-        Unbind();
-    }
 
     void Bind() const override
     {
@@ -60,25 +54,6 @@ template <typename T> class VertexArray : Buffer
         glEnableVertexAttribArray(m_vertex_attribute_count);
         glVertexAttribPointer(m_vertex_attribute_count, static_cast<uint32_t>(VectorType::length()), GL_FLOAT, GL_FALSE, 0, (const void *)0);
         ++m_vertex_attribute_count;
-    }
-
-  private:
-    void AddVertexLayout(const VertexAttributeBuffer<T> &vertex_buffer)
-    {
-        Bind();
-        vertex_buffer.Bind();
-        const VertexLayout &layout = Vertex::Layout();
-        const auto &elements = layout.Elements();
-        uint64_t offset = 0;
-        for (unsigned int i = 0; i < elements.size(); ++i)
-        {
-            const auto &element = elements[i];
-
-            glEnableVertexAttribArray(i);
-            glVertexAttribPointer(i, element.count, element.gl_type, element.normalized, layout.Stride(),
-                                  (const void *)offset);
-            offset += element.count * element.type_bytes;
-        }
     }
 
   private:
