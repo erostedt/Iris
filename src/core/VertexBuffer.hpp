db956387870813b5bd10ec28ce5f8b6995c625b1
erostedt <rostedteric@gmail.com>
2024-10-26 21:56:11 +0200

    refactor: rename variable

--- src/core/VertexBuffer.hpp
+++ src/core/VertexBuffer.hpp
@@ -13,7 +13,7 @@ template <typename T> class VertexAttributeBuffer : Buffer
 {
   public:
     VertexAttributeBuffer(const std::vector<T> &&vertices, const VertexLayout &layout, uint32_t usage=GL_STATIC_DRAW)
-        : m_vertices(vertices), m_layout(layout)
+        : m_data(vertices), m_layout(layout)
     {
         glGenBuffers(1, &m_id);
         Bind();
@@ -31,14 +31,14 @@ template <typename T> class VertexAttributeBuffer : Buffer
 
     VertexAttributeBuffer(VertexAttributeBuffer &&other) noexcept
     {
-        std::swap(m_vertices, other.m_vertices);
+        std::swap(m_data, other.m_data);
         m_layout = other.m_layout;
         m_id = other.m_id;
         other.m_id = 0;
     }
     VertexAttributeBuffer &operator=(VertexAttributeBuffer &&other) noexcept
     {
-        std::swap(m_vertices, other.m_vertices);
+        std::swap(m_data, other.m_data);
         m_layout = other.m_layout;
         m_id = other.m_id;
         other.m_id = 0;
@@ -62,16 +62,16 @@ template <typename T> class VertexAttributeBuffer : Buffer
 
     const std::vector<T> &GetVertices() const
     {
-        return m_vertices;
+        return m_data;
     }
 
     const size_t Size() const
     {
-        return m_vertices.size();
+        return m_data.size();
     }
 
   private:
-    std::vector<T> m_vertices;
+    std::vector<T> m_data;
     VertexLayout m_layout;
     uint32_t m_id{0};
 };
